05f5f888cb2803368044d814f8ac0870ce76afc3
Yusuf Mulhajat <uchup_bell@yahoo.com>
2022-11-15 13:08:29 +0700

    update

--- src/Notifications/MessageDispatched.php
+++ src/Notifications/MessageDispatched.php
@@ -3,12 +3,15 @@
 namespace Uchup07\Messages\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
 use Multicaret\Inbox\Models\Message;
 use Multicaret\Inbox\Models\Thread;
 
-class MessageDispatched extends Notification
+class MessageDispatched extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -39,7 +42,8 @@ class MessageDispatched extends Notification
     {
         return config('laravel-messages.notifications.via', [
             'mail',
-            'array'
+            'database',
+            'broadcast'
         ]);
     }
 
@@ -69,10 +73,27 @@ class MessageDispatched extends Notification
     public function toDatabase($notifiable)
     {
         return [
-            'thread_id' => $this->thread->id,
-            'message_id' => $this->message->id,
+            'title' => $this->message->user->name . ' ' . trans('laravel-messages::la-messages.notification.subject'),
+            'name' => $this->thread->user->name,
+            'email' => $this->thread->user->email,
+            'url' => route(config('laravel-messages.route.name') . 'message.show', $this->thread),
+            'message' => $this->thread->subject,
+            'icon' => 'communication/com002.svg',
             'isReply' => $this->thread->messages()->count() >= 2,
         ];
+    }
+
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'title' => $this->message->user->name . ' ' . trans('laravel-messages::la-messages.notification.subject'),
+            'name' => $this->thread->user->name,
+            'email' => $this->thread->user->email,
+            'url' => route(config('laravel-messages.route.name') . 'message.show', $this->thread),
+            'message' => $this->thread->subject,
+            'icon' => 'communication/com002.svg',
+            'isReply' => $this->thread->messages()->count() >= 2,
+        ]);
     }
 
     /**
@@ -93,7 +114,17 @@ class MessageDispatched extends Notification
             ->success()
             ->subject($this->message->user->name . ' ' . trans('laravel-messages::la-messages.notification.subject') . ' - ' . config('app.name'))
             ->greeting($greeting)
-            ->html($this->message->body)
+            ->line(new HtmlString($this->message->body))
             ->action(trans('laravel-messages::la-messages.notification.button'), $buttonUrl);
+    }
+
+    /**
+     * Get the type of the notification being broadcast.
+     *
+     * @return string
+     */
+    public function broadcastType()
+    {
+        return 'broadcast.message';
     }
 }
